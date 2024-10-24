8aebed74dfc658e59e60cc1cc69d6096b9236522
Neema <neemamwende009@gmail.com>
2024-10-24 18:50:48 +0300

    updates

--- widget/settings.py
+++ widget/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rm+&u^amp&(ps%zo+9v0eb%3k_-9e6ik4ngt70*xrs+883twvt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widgetapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- widgetapp/views.py
+++ widgetapp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+import openai
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.conf import settings
 
-# Create your views here.
+openai.api_key = settings.OPENAI_API_KEY
+
+@api_view(['POST'])
+def chat_with_gpt(request):
+    user_message = request.data.get('message')
+    
+    if not user_message:
+        return Response({"error": "No message provided"}, status=400)
+
+    try:
+        response = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo",
+            messages = [{"role": "user", "content": user_message}]
+        )
+        chatbot_reply = response.choices[0].message['content']
+        return Response({"reply": chatbot_reply})
+    except Exception as e:
+        return Response({"error": str(e)}, status=500)
